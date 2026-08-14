4bb8468aafde46f7f0ffde33f29d3057ce6b2e41
Jaweria Shakeel Ahmed <realmultinet@gmail.com>
2026-08-15 03:41:33 +0500

    refactor: remove legacy build and migration scripts
    Delete redundant data files and administrative utility scripts no longer required for the project.

--- fix_home_div.cjs
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-
-let content = fs.readFileSync('src/pages/Student/components/StudentHome.tsx', 'utf8');
-
-content = content.replace('{/* Right Col: Sidebar */}', '</div>\n        {/* Right Col: Sidebar */}');
-
-fs.writeFileSync('src/pages/Student/components/StudentHome.tsx', content);
